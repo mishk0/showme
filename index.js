c498ab6ca9ee68bb6ecb987d870398c19f12866d
mishk0 <mokrushin.ms@yandex.ru>
2016-07-10 21:05:01 +0300

    init

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
-var config = require('config');
+var config = require('./config.json');
 var request = require('request');
+
+var url = 'https://api.telegram.org/bot' + config.key + '/';
+
+request(
+    {
+        method: 'POST',
+        uri: url + 'sendMessage',
+        json: true,
+        body: {
+            text: '1',
+            chat_id: '36839438'
+        }
+    },
+    function (error, response, body) {
+        console.log(body)
+    }
+);
